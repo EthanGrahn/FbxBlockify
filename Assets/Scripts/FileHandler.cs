640d6a03c71632334459d78e22a0c921ee7cecf7
Ethan <e_q101@live.com>
2018-10-29 20:19:47 -0500

    Initial commit

---
+++ Assets/Scripts/FileHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadObj(string fileLocation)
+	{
+		Mesh holderMesh = new Mesh();
+		ObjImporter newMesh = new ObjImporter();
+		holderMesh = newMesh.ImportFile(fileLocation);
+
+		MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
+		MeshFilter filter = gameObject.AddComponent<MeshFilter>();
+		filter.mesh = holderMesh;
+	}
+}
